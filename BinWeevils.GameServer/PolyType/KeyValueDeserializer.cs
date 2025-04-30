1182f677876fb5c0684f553064d23bbfabdde468
zingballyhoo <zingballyhoo@live.co.uk>
2025-04-30 13:42:14 +0100

    bw/turn: use shared form deserializer for "key/value" message

--- BinWeevils.GameServer/PolyType/KeyValueDeserializer.cs
+++ BinWeevils.GameServer/PolyType/KeyValueDeserializer.cs
@@ -1,68 +1,20 @@
+using ArcticFox.PolyType.FormEncoded;
 using PolyType;
-using PolyType.Abstractions;
-using PolyType.Utilities;
 
 namespace BinWeevils.GameServer.PolyType
 {
     public static class KeyValueDeserializer
     {
-        private static readonly MultiProviderTypeCache m_cache = new MultiProviderTypeCache
-        {
-            ValueBuilderFactory = ctx => new Builder(ctx)
-            // todo: delay
-        };
-        
-        private class Builder(TypeGenerationContext generationContext) : TypeShapeVisitor, ITypeShapeFunc
-        {
-            private ITypeShapeFunc self => generationContext;
-
-            private KeyValueConverter<T> ReEnter<T>(ITypeShape<T> typeShape)
-            {
-                return (KeyValueConverter<T>)self.Invoke(typeShape)!;
-            }
-
-            public object? Invoke<T>(ITypeShape<T> typeShape, object? state = null)
-            {
-                return typeShape.Accept(this);
-            }
-            
-            public override object? VisitObject<T>(IObjectTypeShape<T> type, object? state)
-            {
-                if (type.Type == typeof(string))
-                {
-                    return new StringKeyValueConverter();
-                }
-                if (type.Type == typeof(int))
-                {
-                    return new IntKeyValueConverter();
-                }
-                if (type.Type == typeof(double))
-                {
-                    return new DoubleKeyValueConverter();
-                }
-                if (type.Type == typeof(bool))
-                {
-                    return new BoolKeyValueConverter();
-                }
-                
-                var ctorShape = (IConstructorShape<T, object>)type.Constructor!;
-                var properties = type.Properties
-                    .Select(prop => (KeyValuePropertyConverter<T>)prop.Accept(this)!)
-                    .ToArray();
-                return new ObjectKeyValueConverter<T>(ctorShape.GetDefaultConstructor(), properties);
-            }
-        
-            public override object? VisitProperty<TDeclaringType, TPropertyType>(IPropertyShape<TDeclaringType, TPropertyType> propertyShape, object? state = null)
-            {
-                var propertyConverter = ReEnter(propertyShape.PropertyType);
-                return new KeyValuePropertyConverter<TDeclaringType, TPropertyType>(propertyShape, propertyConverter);
-            }
-        }
-        
         public static T Deserialize<T>(ReadOnlySpan<char> text) where T : IShapeable<T>
         {
-            var converter = (KeyValueConverter<T>)m_cache.GetOrAdd(T.GetShape())!;
-            return converter.Read(text);
+            var options = new FormOptions 
+            {
+                m_keyValueDelimiter = ':',
+                m_nextPropertyDelimiter = ',',
+                m_nextValueDelimiter = '\0', // not supported
+                m_throwOnUnknownFields = true
+            };
+            return options.Deserialize<T>(text);
         }
     }
 }
