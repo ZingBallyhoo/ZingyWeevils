24b29c3ef4330195c0783bdac7a61294bd90c9f4
zingballyhoo <zingballyhoo@live.co.uk>
2025-05-13 20:32:14 +0100

    use KeepAliveInterval + Timeout to hopefully prevent dcs

--- BinWeevils.Server/Program.cs
+++ BinWeevils.Server/Program.cs
@@ -159,6 +159,7 @@ internal static class Program
         app.MapRazorPages().WithStaticAssets().Finally(DisableTracing);
         app.UseWebSockets(new WebSocketOptions
         {
+            KeepAliveInterval = TimeSpan.FromSeconds(15),
             KeepAliveTimeout = TimeSpan.FromSeconds(15)
         });
         
